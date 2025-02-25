241beaf22f2ccbe61e9d91ff14676a5966b16f64
VjAy Reddy Chennadi <vijay.reddy.chennadi@gmail.com>
2025-02-25 23:59:33 +0530

    Create logger.php

---
+++ logger.php
@@ -0,0 +1,8 @@
+<?php
+function logError($message) {
+    $logFile = 'error_log.txt';
+    $timestamp = date("Y-m-d H:i:s");
+    $logMessage = "[$timestamp] ERROR: $message" . PHP_EOL;
+    file_put_contents($logFile, $logMessage, FILE_APPEND);
+}
+?>

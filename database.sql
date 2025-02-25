52b5affa757ea2833a4bc6d9fbbba1f1fa655355
VjAy Reddy Chennadi <vijay.reddy.chennadi@gmail.com>
2025-02-25 16:35:50 +0530

    Create database.sql
    initial checkin

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    mobile VARCHAR(15) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('Customer', 'Artist', 'Student') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

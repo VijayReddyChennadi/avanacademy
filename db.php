5e37233650d3602dc22abd91d4dd033d9c0fc2d9
swathigaddam807 <swathi.gaddam.laddu@gmail.com>
2025-02-26 22:07:44 +0530

    update creds

--- db.php
+++ db.php
@@ -1,12 +1,15 @@
 <?php
+require 'logger.php';
+
 $servername = "localhost";
-$username = "root"; // Change as needed
-$password = ""; // Change as needed
-$dbname = "avan_makeup_academy";
+$username = "u289603047_admin"; // Change as needed
+$password = "Chennadi@Ally24"; // Change as needed
+$dbname = "u289603047_avanacademy";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
+    logError("Database connection failed: " . $conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
